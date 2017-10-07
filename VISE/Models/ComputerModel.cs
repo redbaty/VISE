af64b8097984944c259bd1662c2e3db7fc17decd
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-10-07 00:52:26 -0300

    Added ComputerModel

---
+++ VISE/Models/ComputerModel.cs
@@ -0,0 +1,24 @@
+﻿namespace VISE.Models
+{
+    public class ComputerModel
+    {
+        public string Ip { get; set; }
+        public string Password { get; set; } = "";
+        public bool IsPinned { get; set; }
+
+        public ComputerModel()
+        {
+        }
+
+        public ComputerModel(string ip)
+        {
+            Ip = ip;
+        }
+
+        public ComputerModel(string ip, string password)
+        {
+            Ip = ip;
+            Password = password;
+        }
+    }
+}
